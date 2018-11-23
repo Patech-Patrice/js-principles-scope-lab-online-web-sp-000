1423d686470c0e9a4bcf1f009e042e503ce5d4d4
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-23 18:48:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // Write your solution in this file!
 var customerName = 'bob'
-const leastFavoriteCustomer = 'david'
+
+const leastFavoriteCustomer = 'adam'
+
 function upperCaseCustomerName(){
   customerName = customerName.toUpperCase();
 }
+
 function setBestCustomer(){
   bestCustomer = 'not bob'
 }
+
 function overwriteBestCustomer(){
   bestCustomer = 'maybe bob'
 }
+
 function changeLeastFavoriteCustomer(){
     leastFavoriteCustomer = 'bob';
 }
