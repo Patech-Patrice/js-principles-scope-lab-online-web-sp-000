61c3f7ba828dc36db351ef862bf08d0c0e7d0329
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-11-23 18:18:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    
+     return customerName 
+    }
+
+
+
+function setBestCustomer() {
+  
+}
+
+function overwriteBestCustomer() {
+  var bestCustomer = 'not Susan'
+}
+
+const leastFavoriteCustomer = 'Johnny'
+  function changeLeastFavoritecustomer() {
+      const mostFavoriteCustomer;
+  }
